969312aaf3365b3f88920158f70d453998dc93c2
Frederic Ory <fred@systemike.com>
2020-08-13 21:48:41 -0400

    scheduler component boostrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Paper>
+        <Scheduler
+          data={schedulerData}
         >
-          Learn React
-        </a>
-      </header>
+          <ViewState
+            currentDate={currentDate}
+          />
+          <DayView
+            startDayHour={9}
+            endDayHour={14}
+          />
+          <Appointments />
+        </Scheduler>
+      </Paper>
     </div>
   );
 }
